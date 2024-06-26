9ffb123a8fdea26d088d4496f37cb0bfedca3911
Kevin <kevinstein@gmx.net>
2024-06-26 15:40:04 +0200

    add webchallenges week_05 day 3 part 1

---
+++ week_05/react-state-2_colored-number/src/components/Counter.js
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import ColoredNumber from "./ColoredNumber";
+
+export default function Counter() {
+  const [count, setCount]=useState(0);
+
+  return (
+    <div className="counter">
+      <h1>Colored Number</h1>
+      <ColoredNumber  value={count} />
+      <div className="counter__buttons-container">
+        <button
+          type="button"
+          onClick={() => {setCount(count + 1)}}
+          className="counter__button"
+          aria-label="increment count"
+        >
+          +
+        </button>
+        <button
+          type="button"
+          onClick={() => {setCount(count - 1)}}
+          className="counter__button"
+          aria-label="decrement count"
+        >
+          -
+        </button>
+      </div>
+    </div>
+  );
+}
