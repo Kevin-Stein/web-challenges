6bb9ecf8f874487fc887c4c84869636a0086a282
Kevin <kevinstein@gmx.net>
2024-06-27 13:55:09 +0200

    add web challenges Week_05 day 4 part 1

---
+++ week_05/react-effects-and-fetch_api-status/src/components/StatusCheck/index.js
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import "./StatusCheck.css";
+
+const apiStatusUrl = "https://example-apis.vercel.app/api/status";
+
+export default function StatusCheck() {
+  const [statusIcon, setstatusIcon] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
+
+  async function handleCheckApiStatus() {
+    setIsLoading(false);
+
+    try {
+      if (isLoading) {
+        setstatusIcon("⏳");
+      }
+      const response = await fetch(apiStatusUrl);
+
+      if (response.ok) {
+        setstatusIcon("✅");
+      } else {
+        setstatusIcon("❌");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  return (
+    <article className="status-check">
+      <div className="status-check__wrapper">
+        <h2 className="status-check__heading">Status:</h2>
+        <span className="status-check__icon">{statusIcon}</span>
+      </div>
+      <button
+        type="button"
+        className="status-check__button"
+        onClick={handleCheckApiStatus}
+      >
+        Check API Status
+      </button>
+    </article>
+  );
+}
