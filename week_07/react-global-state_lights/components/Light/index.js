cb6669f3dc9ed56732ae2bdd0ba284ab4a7ff2ba
Kevin <kevinstein@gmx.net>
2024-07-08 14:03:32 +0200

    add web challenge state lights week_07 day 1 part 1

---
+++ week_07/react-global-state_lights/components/Light/index.js
@@ -0,0 +1,22 @@
+
+import { LightButton, Icon, Text, Name, State } from "./Light.styled";
+
+export default function Light({light, onToggleLights }) {
+
+
+  return (
+    <LightButton
+      type="button"
+      onClick={() => {
+        onToggle(light.id);
+      }}
+      $isOn={light.isOn}
+    >
+      <Icon $isOn={light.isOn}>💡</Icon>
+      <Text>
+        <Name>{light.name}</Name>
+        <State>{light.isOn ? "On" : "Off"}</State>
+      </Text>
+    </LightButton>
+  );
+}
